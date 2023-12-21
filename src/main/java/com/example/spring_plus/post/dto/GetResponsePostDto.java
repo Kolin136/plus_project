8fd5b2e644a41947d33579a0d06dbdcf570e026f
Kolin136 <jsowb136@gmail.com>
2023-12-21 15:00:10 +0900

    Refactor:게시글 조회시 해당 게시글의 댓글도 같이 반환

--- src/main/java/com/example/spring_plus/post/dto/GetResponsePostDto.java
+++ src/main/java/com/example/spring_plus/post/dto/GetResponsePostDto.java
@@ -1,12 +1,16 @@
 package com.example.spring_plus.post.dto;
 
+import com.example.spring_plus.comment.dto.GetResponseCommentDto;
 import com.example.spring_plus.post.entity.Post;
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Builder;
 
 @Builder
 public record GetResponsePostDto(
-    Long id, String title, String contents, String username, LocalDateTime createAt) {
+    Long id, String title, String contents, String username, LocalDateTime createAt,
+    Set<GetResponseCommentDto> commentDtoSet) {
 
   public static GetResponsePostDto of(Post post){
     return GetResponsePostDto.builder()
@@ -15,6 +19,7 @@ public record GetResponsePostDto(
         .title(post.getTitle())
         .contents(post.getContents())
         .createAt(post.getCreateAt())
+        .commentDtoSet(post.getCommentSet().stream().map(GetResponseCommentDto::of).collect(Collectors.toSet()))
         .build();
   }
 
