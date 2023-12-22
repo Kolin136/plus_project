4f04b604251cb648dd3a75ab56496bbcef13f5d4
Kolin136 <jsowb136@gmail.com>
2023-12-22 09:48:06 +0900

    Refacor:게시글 삭제 수정

--- src/main/java/com/example/spring_plus/comment/repository/CommentRepository.java
+++ src/main/java/com/example/spring_plus/comment/repository/CommentRepository.java
@@ -3,7 +3,13 @@ package com.example.spring_plus.comment.repository;
 
 import com.example.spring_plus.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+  @Modifying
+  @Query(value = "delete from Comment c where c.post.id = :postId")
+  void deletePostCascadeComments(Long postId);
 
 }
--- src/main/java/com/example/spring_plus/post/repository/PostRepository.java
+++ src/main/java/com/example/spring_plus/post/repository/PostRepository.java
@@ -4,14 +4,9 @@ package com.example.spring_plus.post.repository;
 import com.example.spring_plus.post.entity.Post;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
     List<Post> findAllByOrderByUserUsernameAscCreateAtDesc();
 
-  @Modifying
-  @Query(value = "delete from Comment c where c.post.id = :postId")
-  void deletePostCascadeComments(Long postId);
 }
